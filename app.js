fa6708b9e81424775fabe43d0b80a8040d390b8b
Agustin F <agustinfratti2013@gmail.com>
2026-06-10 01:35:52 -0300

    fix cors for frontend

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 import express from "express";
+import cors from "cors";
+
 import routes from "./src/server/v1/routes/index.js";
 import { errorMiddleware } from "./src/server/v1/middleware/error.middleware.js";
 import { rutaNoEncontradaMiddleware } from "./src/server/v1/middleware/not-found.middleware.js";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://obligatorio-parte2-dfs.vercel.app",
+    ],
+  }),
+);
 
 app.use(express.json());
 app.use(routes);
